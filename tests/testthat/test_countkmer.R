574f222f0eff6372258343a80fa88f91f4364635
elenawu01 <elena.wu2000@gmail.com>
2022-01-25 03:31:27 -0800

    added progress import

--- tests/testthat/test_countkmer.R
+++ tests/testthat/test_countkmer.R
@@ -2,9 +2,13 @@ context("Count k-mers in nucleotide sequence")
 library(LymphoSeqTest)
 
 test_that("Number of k-mers are counted correctly", {
-    junction <- "ACCTAGGT"
+    junction <- "ATCGATCAC"
     study_table <- tibble(junction)
-    ktable <- LymphoSeqTest::countKmer(study_table = study_table, k = 5)
+    ktable <- LymphoSeqTest::countKmer(study_table = study_table, k = 3)
     num_rows <- base::nrow(ktable)
-    expect_equal(num_rows, 4)
+    Kmer <- c("ATC", "TCG", "CGA", "GAT", "TCA", "CAC")
+    Count <- c(2, 1, 1, 1, 1, 1)
+    kmer_table <- tibble(Kmer, Count)
+    expect_equal(num_rows, 6)
+    expect_equal(ktable, kmer_table)
 })
