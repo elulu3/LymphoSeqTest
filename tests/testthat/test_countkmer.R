7f6b01476c8675d9a1513b6dee49db1506f3ccd3
elenawu01 <elena.wu2000@gmail.com>
2022-01-25 03:00:14 -0800

    added to description

--- tests/testthat/test_countkmer.R
+++ tests/testthat/test_countkmer.R
@@ -1,7 +1,7 @@
 context("Count k-mers in nucleotide sequence")
 library(LymphoSeqTest)
 
-test_that("K-mer counts are summarized correctly", {
+test_that("Number of k-mers are counted correctly", {
     junction <- "ACCTAGGT"
     study_table <- tibble(junction)
     ktable <- LymphoSeqTest::countKmer(study_table = study_table, k = 5)
